dfab7a6e1a954cdf013fea026aa295427acb7831
taxusyew <taxusyew@gmail.com>
2016-09-28 11:31:33 +0800

    update

--- generators/app/index.js
+++ generators/app/index.js
@@ -164,7 +164,7 @@ module.exports = yeoman.Base.extend({
     );
 
     this.fs.copy(
-      this.templatePath('.gitignore'),
+      this.templatePath('gitignore.txt'),
       this.destinationPath('.gitignore')
     );
 
